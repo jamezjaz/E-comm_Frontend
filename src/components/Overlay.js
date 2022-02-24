5e92d9e761bc0e32dd26ec815b04ca6198f1e4e6
Jamezjaz <jamezjaz@gmail.com>
2022-02-24 15:51:23 +0100

    fix: Removes log statement

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -55,7 +55,7 @@ class Overlay extends React.Component {
                       <SubTitle>{item.name}</SubTitle>
                       <p>{price(item.prices, label)}</p>
                       <div>
-                        {console.log('Item', item)}
+                        {/* {console.log('Item', item)} */}
                         {item.category === 'clothes' && 
                           <>
                             {item.attributes.map(attr => attr.items.map(option => (
